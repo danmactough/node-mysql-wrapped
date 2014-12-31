4ae5d6bd9d3d5d395fc09a055bc34ddac699f83b
Dan MacTough <danmactough@gmail.com>
2014-12-31 14:29:48 -0500

    Add tests for ensuring timed out connections are removed from the pool

--- test/index.js
+++ test/index.js
@@ -67,6 +67,89 @@ describe('pool', function () {
     }).then(done, done);
   });
 
+  it('can set the wait_timeout', function (done) {
+    var pool = mysql.createPool({
+      host: '127.0.0.1',
+      database: testDB,
+      user: 'root',
+      password: '',
+      connectionLimit: 1
+    });
+    co(function* () {
+      yield pool.query("SET wait_timeout=60");
+      var results = yield pool.query("SHOW VARIABLES LIKE 'wait_timeout'");
+      debug(results);
+      assert.strictEqual(results.length, 1);
+      assert.equal(results[0].Variable_name, 'wait_timeout');
+      assert.equal(results[0].Value, 60);
+    }).then(done, done);
+  });
+
+  it('timed out connections are removed from the pool', function (done) {
+    this.timeout('3000');
+    this.slow('2500');
+
+    var connectionIds = [];
+    var killedConnections = [];
+
+    var pool = mysql.createPool({
+      host: '127.0.0.1',
+      database: testDB,
+      user: 'root',
+      password: '',
+      connectionLimit: 2
+    });
+
+    pool.on('connection', function (conn) {
+      debug('got connection:', conn.threadId);
+      connectionIds.push(conn.threadId);
+      debug('connectionIds length', connectionIds.length);
+      conn.on('error', function (err) {
+        if (err.fatal && err.code === 'PROTOCOL_CONNECTION_LOST') {
+          killedConnections.push(conn.threadId);
+          assert.strictEqual(conn._pool, null);
+          debug('killed connection:', conn.threadId);
+          debug('killedConnections length', killedConnections.length);
+        }
+      });
+    });
+
+    co(function* () {
+      // conn 1
+      yield pool.query("SET wait_timeout=1");
+      assert.equal(pool._allConnections.length, 1);
+      assert.equal(pool._freeConnections.length, 1);
+      assert.equal(pool._acquiringConnections.length, 0);
+      assert.equal(pool._connectionQueue.length, 0);
+      assert.equal(connectionIds.length, 1);
+      assert.equal(killedConnections.length, 0);
+      // conn 1
+      var results = yield pool.query("SELECT 1");
+      debug(results);
+      assert.equal(pool._allConnections.length, 1);
+      assert.equal(pool._freeConnections.length, 1);
+      assert.equal(pool._acquiringConnections.length, 0);
+      assert.equal(pool._connectionQueue.length, 0);
+      assert.equal(connectionIds.length, 1);
+      assert.equal(killedConnections.length, 0);
+      // conn 2!
+      debug('Starting 1500ms timeout');
+      setTimeout(function () {
+        debug('Finished 1500ms timeout');
+        co(function* () {
+          var results = yield pool.query("SELECT 1");
+          debug(results);
+          assert.equal(pool._allConnections.length, 1);
+          assert.equal(pool._freeConnections.length, 1);
+          assert.equal(pool._acquiringConnections.length, 0);
+          assert.equal(pool._connectionQueue.length, 0);
+          assert.equal(connectionIds.length, 2);
+          assert.equal(killedConnections.length, 1);
+        }).then(done, done);
+      }, 1500);
+    });
+  });
+
 });
 
 describe('pool cluster', function () {
